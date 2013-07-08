35d2e1865954a1045caff890b9de6f054644106c
Amy de Buitléir <amy@nualeargais.ie>
2013-07-08 14:51:52 +0100

    v1.3

--- test/Codec/GrayQC.hs
+++ test/Codec/GrayQC.hs
@@ -1,21 +1,18 @@
-{-# LANGUAGE UnicodeSyntax #-}
-
 module Codec.GrayQC
   (
     test
   ) where
 
 import Codec.Gray
-import Data.Eq.Unicode ((≡),(≠))
 import Data.List (nub)
 import Test.Framework as TF (Test, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck ((==>), Property, Gen, Arbitrary, arbitrary, choose, 
   property, sized)
 
-prop_successive_values_differ_in_one_place1 ∷ Int → Property
+prop_successive_values_differ_in_one_place1 :: Int -> Property
 prop_successive_values_differ_in_one_place1 k = k > 0 ==>
-  nub (diffCounts xs) ≡ [1]
+  nub (diffCounts xs) == [1]
     where xs = grayCodes k'
           k' = min 10 k -- avoid long tests
 
@@ -31,20 +28,20 @@ sizedTestParms n = do
 instance Arbitrary TestParms where
   arbitrary = sized sizedTestParms
   
-prop_successive_values_differ_in_one_place2 ∷ TestParms → Property
+prop_successive_values_differ_in_one_place2 :: TestParms -> Property
 prop_successive_values_differ_in_one_place2 (TestParms ds k) = property $
-  nub (diffCounts xs) ≡ [1]
+  nub (diffCounts xs) == [1]
     where xs = naryGrayCodes ds k
 
-diffCounts ∷ Eq b ⇒ [[b]] → [Int]
+diffCounts :: Eq b => [[b]] -> [Int]
 diffCounts [] = []
 diffCounts [_] = []
 diffCounts (x1:x2:xs) = (diffCount x1 x2) : diffCounts (x2:xs)
 
-diffCount ∷ Eq b ⇒ [b] → [b] → Int
-diffCount as bs = length $ filter (\x → x) $ zipWith (≠) as bs
+diffCount :: Eq b => [b] -> [b] -> Int
+diffCount as bs = length $ filter (\x -> x) $ zipWith (/=) as bs
 
-test ∷ Test
+test :: Test
 test = testGroup "Codec.GrayQC"
   [
     testProperty "prop_successive_values_differ_in_one_place1"
--- test/Main.hs
+++ test/Main.hs
@@ -1,15 +1,14 @@
-{-# LANGUAGE UnicodeSyntax #-}
 module Main where
 
 import Codec.GrayQC ( test )
 
 import Test.Framework as TF ( defaultMain, Test )
 
-tests ∷ [TF.Test]
+tests :: [TF.Test]
 tests = 
   [ 
     Codec.GrayQC.test
   ]
 
-main ∷ IO ()
+main :: IO ()
 main = defaultMain tests
