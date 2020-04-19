0f73d7be291bb76bb81c3202ac5b9a469ec28676
Amy de Buitléir <amy@nualeargais.ie>
2020-04-19 22:21:26 +0100

    revamp nix build

--- default.nix
+++ default.nix
@@ -1,3 +1,15 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.haskellPackages.callCabal2nix "gray-extended" ./. {}
+{ mkDerivation, base, QuickCheck, stdenv, test-framework
+, test-framework-quickcheck2
+}:
+mkDerivation {
+  pname = "gray-extended";
+  version = "1.5.8";
+  src = ./.;
+  libraryHaskellDepends = [ base ];
+  testHaskellDepends = [
+    base QuickCheck test-framework test-framework-quickcheck2
+  ];
+  homepage = "https://github.com/mhwombat/gray-extended";
+  description = "Gray encoding schemes";
+  license = stdenv.lib.licenses.bsd3;
+}
--- shell.nix
+++ shell.nix
@@ -1,1 +1,33 @@
-(import ./. {}).env
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, QuickCheck, stdenv, test-framework
+      , test-framework-quickcheck2
+      }:
+      mkDerivation {
+        pname = "gray-extended";
+        version = "1.5.8";
+        src = ./.;
+        libraryHaskellDepends = [ base ];
+        testHaskellDepends = [
+          base QuickCheck test-framework test-framework-quickcheck2
+        ];
+        homepage = "https://github.com/mhwombat/gray-extended";
+        description = "Gray encoding schemes";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
