a629bdff8522c80251911bd505ba55c25cbba910
Amy de Buitléir <amy@nualeargais.ie>
2020-04-25 18:45:06 +0100

    added profiling

--- project.nix
+++ project.nix
@@ -9,6 +9,8 @@ mkDerivation {
   testHaskellDepends = [
     base QuickCheck test-framework test-framework-quickcheck2
   ];
+  enableLibraryProfiling = true;
+  enableExecutableProfiling = true;
   homepage = "https://github.com/mhwombat/gray-extended";
   description = "Gray encoding schemes";
   license = stdenv.lib.licenses.bsd3;
