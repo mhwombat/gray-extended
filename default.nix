dd72d7f890afe06c005b9488487955d81f534aff
Amy de Buitléir <amy@nualeargais.ie>
2020-05-16 23:26:05 +0100

    use hs-nix-template

--- default.nix
+++ default.nix
@@ -1,15 +1,47 @@
-{ mkDerivation, base, QuickCheck, stdenv, test-framework
-, test-framework-quickcheck2
-}:
-mkDerivation {
-  pname = "gray-extended";
-  version = "1.5.8";
-  src = ./.;
-  libraryHaskellDepends = [ base ];
-  testHaskellDepends = [
-    base QuickCheck test-framework test-framework-quickcheck2
-  ];
-  homepage = "https://github.com/mhwombat/gray-extended";
-  description = "Gray encoding schemes";
-  license = stdenv.lib.licenses.bsd3;
+{ compiler ? "ghc883" }:
+
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+
+  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = hself: hsuper: {
+      "gray-extended" =
+        hself.callCabal2nix
+          "gray-extended"
+          (gitignore ./.)
+          {};
+    };
+  };
+
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p."gray-extended"
+    ];
+    buildInputs = with pkgs.haskellPackages; [
+      myHaskellPackages.cabal-install
+      ghcid
+      ormolu
+      hlint
+      (import sources.niv {}).niv
+      pkgs.nixpkgs-fmt
+    ];
+    withHoogle = true;
+  };
+
+  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages."gray-extended");
+
+  docker = pkgs.dockerTools.buildImage {
+    name = "gray-extended";
+    config.Cmd = [ "${exe}/bin/gray-extended" ];
+  };
+in
+{
+  inherit shell;
+  inherit exe;
+  inherit docker;
+  inherit myHaskellPackages;
+  "gray-extended" = myHaskellPackages."gray-extended";
 }
