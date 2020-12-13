2a909d505501ba77191331c5e90fb4f019c81919
Amy de Buitléir <amy@nualeargais.ie>
2020-12-13 19:41:41 +0000

    streamlined nix build

--- default.nix
+++ default.nix
@@ -1,47 +1,10 @@
-{ compiler ? "ghc883" }:
-
-let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
-
-  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
-
-  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
-    overrides = hself: hsuper: {
-      "gray-extended" =
-        hself.callCabal2nix
-          "gray-extended"
-          (gitignore ./.)
-          {};
-    };
-  };
-
-  shell = myHaskellPackages.shellFor {
-    packages = p: [
-      p."gray-extended"
-    ];
-    buildInputs = with pkgs.haskellPackages; [
-      myHaskellPackages.cabal-install
-      ghcid
-      ormolu
-      hlint
-      (import sources.niv {}).niv
-      pkgs.nixpkgs-fmt
-    ];
-    withHoogle = true;
-  };
-
-  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages."gray-extended");
-
-  docker = pkgs.dockerTools.buildImage {
-    name = "gray-extended";
-    config.Cmd = [ "${exe}/bin/gray-extended" ];
-  };
-in
-{
-  inherit shell;
-  inherit exe;
-  inherit docker;
-  inherit myHaskellPackages;
-  "gray-extended" = myHaskellPackages."gray-extended";
-}
+let 
+  pkgs = import <nixpkgs> { };
+in 
+  pkgs.haskellPackages.developPackage {
+    root = ./.;
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+        [ cabal-install
+        ]);
+  }
